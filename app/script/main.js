929d2c83c5af8f265c37c29f7fb6500691860e63
ciming <xuxcf2010@163.com>
2017-07-08 19:11:54 +0800

    添加滚动监听

--- app/script/main.js
+++ app/script/main.js
@@ -3,10 +3,31 @@ $(window).on('load', function () {
 })
 
 $(function () {
+
   // STICKY MAINMENU
   $("#mainmenu-area").sticky({
     topSpacing: 0
   });
-  //percircle
+
+  // percircle
   $(".c100").percircle();
+
+  // nav scroll activr
+
+  $('body').scrollspy({
+    target: '#header-navbar',
+    offset: 65
+  });
+
+  // nav scroll
+
+  $('ul#nav li a[href^="#"], .rotate-title a, .push-menu ul li a, a.navbar-brand,a.scrolltotop,.call-to-action a,.home-button a').on('click', function (event) {
+    var id = $(this).attr("href");
+    var offset = 40;
+    var target = $(id).offset().top - offset;
+    $('html, body').animate({
+      scrollTop: target
+    }, 1500, "easeInOutExpo");
+    event.preventDefault();
+  });
 })
